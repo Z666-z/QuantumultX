a16ece49af5440f1f20d0b4864f603aabefb7ab6
Z666-z <66162405+Z666-z@users.noreply.github.com>
2022-11-06 15:14:11 +0800

    Add files via upload

---
+++ Crack/Nicegrampro.js
@@ -0,0 +1,17 @@
+/******************************
+
+脚本功能：Nicegram: Unlimited Text+解锁订阅
+下载地址：https://is.gd/7OPpId
+软件版本：1.1.2
+
+*******************************
+
+[rewrite_local]
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/Z666-z/QuantumultX/main/Crack/Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
+
+
+*******************************/
