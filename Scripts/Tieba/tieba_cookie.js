5c36156b4e98e69522a783c86d3da74c843318d1
Z666-z <66162405+Z666-z@users.noreply.github.com>
2022-11-07 09:07:50 +0800

    Update tieba_cookie.js

--- Scripts/Tieba/tieba_cookie.js
+++ Scripts/Tieba/tieba_cookie.js
@@ -10,4 +10,4 @@ hostname = c.tieba.baidu.com
 
 ^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/Z666-z/QuantumultX/main/Scripts/Tieba/tieba_signin.js
 ^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/Z666-z/QuantumultX/main/Scripts/Tieba/tieba_signin.js
-^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/Z666-z/main/Scripts/Tieba/tieba_signin.js
+^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/Z666-z/QuantumultX/main/Scripts/Tieba/tieba_signin.js
