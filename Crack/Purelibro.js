89968e94f7fc52b9129d1083848bf3210c255ddb
Z666-z <66162405+Z666-z@users.noreply.github.com>
2024-06-19 16:32:43 +0800

    Purelibro.js

--- Crack/Purelibro.js
+++ Crack/Purelibro.js
@@ -3,20 +3,16 @@
 #!unlockapplist=https://appraven.net/collection/77299969
 #!author=𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑
 #!tgchannel=https://t.me/ddgksf2021
-#!moduleUrl=https://github.com/ddgksf2013/Modules/raw/main/RevenueCat.Vip.sgmodule
+#!moduleUrl=https://github.com/Z666-z/Quantumult/raw/main/Crack/Purelibro.js
 #!howtouse=小火箭、Surge用户直接添加模块，QuantumultX用户添加重写时打开解析器
 
 [Script]
 # ～ RevenueCat@ddgksf2013
-RevenueCatVip = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path= https://raw.githubusercontent.com/Z666-z/QuantumultX/main/Crack/Purelibro.js, requires-body=true, max-size=-1, timeout=60
-RevenueCatDeleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path= https://raw.githubusercontent.com/Z666-z/QuantumultX/main/Crack/Purelibro.js, timeout=60
+RevenueCatVip = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=h https://github.com/Z666-z/Quantumult/raw/main/Crack/Purelibro.js, requires-body=true, max-size=-1, timeout=60
+RevenueCatDeleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path= https://github.com/Z666-z/Quantumult/raw/main/Crack/Purelibro.js, timeout=60
 
 [MITM]
 hostname = %APPEND% api.revenuecat.com
-
-
-
-
 
 
 
